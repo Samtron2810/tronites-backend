4f997cb755e5caed8b3bd196e94157f49f9f150b
samtron <freegram730@gmail.com>
2026-09-12 15:48:23 +0100

    fix: restore shadowRankSchema import that crashed adminRoutes at boot

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -4,8 +4,8 @@ import protect from "../middleware/authMiddleware.js";
 import requireAdmin from "../middleware/requireAdmin.js";
 import requireModerator from "../middleware/requireModerator.js";
 import requirePermission from "../middleware/requirePermission.js";
-import { validate } from "../utils/validators.js";
 import {
+  validate,
   updateRoleSchema,
   suspendUserSchema,
   banUserSchema,
@@ -15,6 +15,7 @@ import {
   addModeratorNoteSchema,
   grantVerificationSchema,
   revokeVerificationSchema,
+  shadowRankSchema,
 } from "../utils/validators.js";
 import {
   listUsersForAdmin,
